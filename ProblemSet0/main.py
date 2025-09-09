6e655ea3ccee49ed3bb2a4ca6e1dc48d017765a1
ak-cal <angelkylecalacar@gmail.com>
2025-09-09 01:37:58 -0700

    Set 0 done but needs revision

--- ProblemSet0/main.py
+++ ProblemSet0/main.py
@@ -1,5 +1,3 @@
-import emoji
-
 def main():
     choice = input("What would you like to do? " \
     "\n1. Indoor.py" \
@@ -16,10 +14,15 @@ def main():
             phrase = input("Type a phrase/sentence: ")
             playback(phrase)
         case "3":
-            emoticon = input("Enter a text-based emoji")
+            emoticon = input("Enter a text-based emoji: ")
             emoticon = convert(emoticon)
 
             print(emoticon)
+        case "4":
+            mass = int(input("Enter Mass: "))
+            einstein(mass)
+        case "5":
+            print("hello")
 
 
 def indoor():
@@ -39,7 +42,11 @@ def convert(emoji):
         emoji = emoji.replace(word, emoticon)
     return emoji
 
-def einstein():
-    print("hello")
+def einstein(m):
+    c = 300000000
+    E = m*c**2
+    print(E)
 
+def calculator():
+    print("hello")
 main()
