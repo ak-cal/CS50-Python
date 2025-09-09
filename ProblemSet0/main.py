3ebc35388deb17ee733c8b839652294de64817e7
ak-cal <angelkylecalacar@gmail.com>
2025-09-09 01:09:54 -0700

    Initial Commit

---
+++ ProblemSet0/main.py
@@ -0,0 +1,45 @@
+import emoji
+
+def main():
+    choice = input("What would you like to do? " \
+    "\n1. Indoor.py" \
+    "\n2. Playback Speed" \
+    "\n3. Making Faces" \
+    "\n4. Einstein" \
+    "\n5. Tip Calculator" \
+    "\nChoice: ")
+
+    match choice:
+        case "1":     
+            indoor()
+        case "2":
+            phrase = input("Type a phrase/sentence: ")
+            playback(phrase)
+        case "3":
+            emoticon = input("Enter a text-based emoji")
+            emoticon = convert(emoticon)
+
+            print(emoticon)
+
+
+def indoor():
+    to_lower = input("Type something in caps (HELLO): ").lower()
+    print(to_lower)
+
+def playback(phrase):
+    phrase = phrase.replace(" ", "...")
+    print(phrase)
+
+def convert(emoji):
+    emoji_dict = {
+        ":)": "🙂",
+        ":(": "🙁"
+    }
+    for word, emoticon in emoji_dict.items():
+        emoji = emoji.replace(word, emoticon)
+    return emoji
+
+def einstein():
+    print("hello")
+
+main()
